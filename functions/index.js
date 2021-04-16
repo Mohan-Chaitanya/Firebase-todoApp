fb4de802bc84384f5430de9674baa667e0520369
Mohan Chaitanya <mohanchaitanya8@outlook.com>
2021-04-16 01:45:55 -0400

    First commit

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,5 @@
-const functions = require("firebase-functions");
-const admin = require('firebase-admin');
-const db = admin.initializeApp().firestore();
+// const functions = require("firebase-functions");
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -8,14 +7,3 @@ const db = admin.initializeApp().firestore();
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
-
-exports.addTodo = functions.https.onCall(async (data, context) => {
-    const todosRef = db.collection(`/users/${context.auth.uid}/todos`);
-    const todosSnapshot = await todosRef.get();
-    const todos = todosSnapshot.docs.map((snapshot) => snapshot.data());
-    const exists = todos.some((todo) => todo.text === data.text);
-
-    if (!exists) {
-        await todosRef.add(data);
-    }
-});
